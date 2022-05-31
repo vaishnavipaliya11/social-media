d0f600f75171b0ccd6f2cf7408579dd4b347d388
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-05-31 10:47:26 +0530

    style: styled profile page

--- src/Pages/profile/Profile.jsx
+++ src/Pages/profile/Profile.jsx
@@ -1,42 +1,45 @@
 import React from "react";
-
+import "./Profile.css"
+import { Avatar, Box, Button, Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 export const Profile = () => {
   return (
    
-      <div className="user-profile">
-        <img
-          className="profile-img"
+      <Box className="user-profile">
+        <Avatar
+        size='2xl'
+          // className="profile-img"
           src="https://resize.indiatvnews.com/en/resize/newbucket/715_-/2016/05/shaktiman-1462557537.jpg"
         />
-        <div className="dis-col">
+        <Box className="dis-col">
           {" "}
-          <h3 className="profile-user-name">Vaishnavi paliya</h3>
-          <p className="profile-user-id">@Vaishnavipaliya</p>
-        </div>
-        <button className="edit-profile">Edit Profile</button>
-        <div className="profile-bio">
+          <Heading as='h2' size='lg' className="profile-user-name pd-med">Vaishnavi paliya</Heading>
+          <p className="profile-user-id pd-xs">@Vaishnavipaliya</p>
+        </Box>
+        <Button className="edit-profile">Edit Profile</Button>
+        <Box className="profile-bio pd-med">
           i'm FrontEnd Developer i build fully functional FrontEnd Sites with
           React JS / Redux . i'll suggest you to have a look at my Projects in
           "Projects" section
-        </div>
-        <a>www.Vaishnavipaliya.com</a>
-        <div>
-          <div className="dis-row user-stats">
-            <div className="dis-col">
-              <h4>02</h4>
-              <h4>following</h4>
-            </div>
-            <div className="dis-col">
-              <h4>02</h4>
-              <h4>following</h4>
-            </div>
-            <div className="dis-col">
-              <h4>02</h4>
-              <h4>following</h4>
-            </div>
-          </div>
-        </div>
-      </div>
+        </Box>
+        <a href="https://vaishnavirpaliya-portfolio.netlify.app/">https://vaishnavirpaliya-portfolio.netlify.app/</a>
+        <Box>
+          <Box className="dis-row user-stats">
+            <Box className="dis-col">
+              <Heading as='h5' size='sm'>02</Heading>
+              <Heading as='h5' size='sm'>following</Heading>
+            </Box>
+            <Box className="dis-col">
+              <Heading as='h5' size='sm'>02</Heading>
+              <Heading as='h5' size='sm'>following</Heading>
+            </Box>
+            <Box className="dis-col">
+              <Heading as='h5' size='sm'>02</Heading>
+              <Heading as='h5' size='sm'>following</Heading>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
    
   );
 };
